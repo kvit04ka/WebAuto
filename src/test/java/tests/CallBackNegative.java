484c83b5506536b6acfcfdf94d43f26b2eaff0dd
Natalia <nataozira@gmail.com>
2020-02-17 11:38:15 +0200

    CallBack commit

---
+++ src/test/java/tests/CallBackNegative.java
@@ -0,0 +1,54 @@
+package test.java.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CallBackNegative {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://iteaua-develop.demo.gns-it.com/");
+
+        By callback = By.className("callback-btn");
+        WebElement callbackEl = driver.findElement(callback);
+        WebDriverWait waitForPresence = new WebDriverWait(driver, 15);
+        waitForPresence.until(ExpectedConditions.elementToBeClickable(callback));
+        callbackEl.click();
+
+        WebElement submitBtn = driver.findElement(( By.xpath("//div[@class='b-header-contacte-phone-block']//input[@type='submit']") ));
+        waitForPresence.until(ExpectedConditions.elementToBeClickable(submitBtn));
+        submitBtn.click();
+
+        WebElement nameInput = driver.findElement(By.xpath("//input[@name='name']"));
+        waitForPresence.until(ExpectedConditions.attributeContains(nameInput,"style", "border-color: red;" ));
+        String actualNameStyle = nameInput.getAttribute("style");
+
+        WebElement phoneInput = driver.findElement(By.xpath("//input[@name='phone']"));
+        String actualPhoneStyle = phoneInput.getAttribute("style");
+
+        String expectedValue = "border-color: red;";
+        assertEquals(expectedValue, actualNameStyle, actualPhoneStyle);
+
+
+        }
+
+        @AfterMethod
+        public void tearDown(){
+        driver.quit();
+
+
+    }
+}
