5ca0607549c572f4006f35964a497471a902de18
Natalia <nataozira@gmail.com>
2020-02-24 15:29:55 +0200

    hometask6

--- src/test/java/tests/EveningCourses.java
+++ src/test/java/tests/EveningCourses.java
@@ -31,45 +31,26 @@ public class EveningCourses extends BaseTest {
                 .openEveningCourses()
                 .openCourses();
 
-        String coursesExpected[] = {
-                "Тестування",
-                "Frontend development",
-                "JS development",
-                "Веб-дизайн",
-                "PHP",
-                "Java programming",
-                "Python",
-                "Data Science/Machine Learning",
-                "C# /.NET development",
-                "C++",
-                "Game Development",
-                "DEVOPS",
-                "Digital Marketing",
-                "Управління персоналом",
-                "Управління проектами",
-                "Mobile development",
-                "Відеомонтаж",
-                "Cisco",
-                "Go development",
-                "Кібербезпека",
-                "Менеджмент"
-        };
+        String[] coursesExpected = {"Тестування", "Frontend development", "JS development", "Веб-дизайн", "PHP", "Java programming", "Python",
+                "Data Science/Machine Learning", "C# /.NET development", "C++", "Game Development", "DEVOPS", "Digital Marketing",
+                "Управління персоналом", "Управління проектами", "Mobile development", "Відеомонтаж", "Cisco", "Go development", "Кібербезпека",
+                "Менеджмент"};
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("(//*[contains(@class, 'grid_3')]//h2)"))));
         List<WebElement> courseElements = driver.findElements(By.xpath("(//*[contains(@class, 'grid_3')]//h2)"));
         List<String> coursesActual = new ArrayList<String>();
-
 
  //    for (WebElement el : courseElements) {
  //              System.out.println("\"" + el.getText() + "\"" + ", ");
   //         }
 
         for (WebElement el : courseElements) {
-            el.getText();
+            coursesActual.add(el.getText());
+        }
             for (String course : coursesExpected) {
                 boolean isContains = coursesActual.contains(course);
                 assertTrue(isContains, String.format("Expected course '%s' to be present on the page", course));
             }
         }
-    }}
+    }
 
 
